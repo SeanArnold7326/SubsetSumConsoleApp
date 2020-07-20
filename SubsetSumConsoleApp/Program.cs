6ca2c17b8c5055db5f5c676805fe658e89f8937b
SeArnold7326 <searnold7326@gmail.com>
2020-07-20 13:55:17 -0400

    Changed loop to handle checking all numbers

--- SubsetSumConsoleApp/Program.cs
+++ SubsetSumConsoleApp/Program.cs
@@ -65,36 +65,78 @@ namespace SubsetSumConsoleApp
 
                 List<Int32> currentNumbers = new List<Int32>();
 
+                Int32 curNum = set[i];
+                set.RemoveAt(i);
+                set.Add(curNum);
 
-                int curSum = set[i];
-                currentNumbers.Add(set[i]);
-                for (int j = 0; j < set.Count; j++)
+                bool exhausted = false;
+                int pointer = 0;
+
+                while (!exhausted)
                 {
-                    if (i != j)
+                    int curSum = curNum;
+                    currentNumbers.Add(curNum);
+                    int counter = 0;
+
+                    while(counter != set.Count)
                     {
-                        if (curSum + set[j] > sum)
+                        if (curSum + set[pointer] > sum)
                         {
+                            currentNumbers.Clear();
                             continue;
-                        }
-                        else if (curSum + set[j] < sum)
+                        } else if(curSum + set[pointer] < sum)
                         {
-                            currentNumbers.Add(set[j]);
-                            curSum += set[j];
+                            currentNumbers.Add(set[pointer]);
+                            curSum += set[pointer];
+                            counter++;
                         }
                         else
                         {
-                            currentNumbers.Add(set[j]);
-                            bool alreadyExists = false;
-                            foreach(List<Int32> existingSet in numSums)
-                            {
-                                alreadyExists = (existingSet.All(currentNumbers.Contains)); 
-                            }
-                            if (!alreadyExists) {
-                                numSums.Add(currentNumbers);
-                            }
-                            break;
+                            currentNumbers.Add(set[pointer]);
+                            numSums.Add(currentNumbers);
+                            counter = set.Count;
                         }
                     }
+                    if (pointer == set.Count)
+                    {
+                        exhausted = true;
+                    }
+                    else
+                    {
+                        pointer++;
+                    }
+                    //for (int j = 0; j < set.Count; j++)
+                    //{
+                    //    if (i != j)
+                    //    {
+                    //        if (curSum + set[j] > sum)
+                    //        {
+                    //            currentNumbers.Clear();
+                    //            currentNumbers.Add(set[i]);
+                    //            continue;
+                    //        }
+                    //        else if (curSum + set[j] < sum)
+                    //        {
+                    //            currentNumbers.Add(set[j]);
+                    //            curSum += set[j];
+                    //        }
+                    //        else
+                    //        {
+                    //            currentNumbers.Add(set[j]);
+                    //            numSums.Add(currentNumbers);
+                    //            //bool alreadyExists = false;
+                    //            //foreach (List<Int32> existingSet in numSums)
+                    //            //{
+                    //            //    alreadyExists = (existingSet.All(currentNumbers.Contains));
+                    //            //}
+                    //            //if (!alreadyExists)
+                    //            //{
+                    //            //    numSums.Add(currentNumbers);
+                    //            //}
+                    //            break;
+                    //        }
+                    //    }
+                    //}
                 }
             }
 
