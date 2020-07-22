791ef49b7813807ed4bd4ba00949fd0d2b5064ca
SeArnold7326 <searnold7326@gmail.com>
2020-07-22 14:05:14 -0400

    Added index to loop to keep differentiate from counter. Need to fix issue with current number being added twice.

--- SubsetSumConsoleApp/Program.cs
+++ SubsetSumConsoleApp/Program.cs
@@ -70,73 +70,51 @@ namespace SubsetSumConsoleApp
                 set.Add(curNum);
 
                 bool exhausted = false;
-                int pointer = 0;
+                int startPointer = 0;
 
                 while (!exhausted)
                 {
+                    int index = startPointer;
                     int curSum = curNum;
                     currentNumbers.Add(curNum);
                     int counter = 0;
 
-                    while(counter != set.Count)
-                    {
-                        if (curSum + set[pointer] > sum)
-                        {
-                            currentNumbers.Clear();
-                            continue;
-                        } else if(curSum + set[pointer] < sum)
-                        {
-                            currentNumbers.Add(set[pointer]);
-                            curSum += set[pointer];
-                            counter++;
-                        }
-                        else
-                        {
-                            currentNumbers.Add(set[pointer]);
-                            numSums.Add(currentNumbers);
-                            counter = set.Count;
-                        }
-                    }
-                    if (pointer == set.Count)
+                    startPointer++;
+
+                    if (startPointer == set.Count - 1)
                     {
                         exhausted = true;
+                        break;
                     }
-                    else
+
+                    while (counter != set.Count - 1)
                     {
-                        pointer++;
+                        if (curSum + set[index] > sum)
+                        {
+                            currentNumbers.Clear();
+                            counter = set.Count;
+                            break;
+
+                        } else if(curSum + set[index] < sum)
+                        {
+                            currentNumbers.Add(set[index]);
+                            curSum += set[index];
+                            if (index + 1 == set.Count - 1)
+                            {
+                                index = 0;
+                            } else
+                            {
+                                index++;
+                            }
+                            counter++;
+                        } else
+                        {
+                            currentNumbers.Add(set[index]);
+                            numSums.Add(currentNumbers);
+                            counter = set.Count;
+                            break;
+                        }
                     }
-                    //for (int j = 0; j < set.Count; j++)
-                    //{
-                    //    if (i != j)
-                    //    {
-                    //        if (curSum + set[j] > sum)
-                    //        {
-                    //            currentNumbers.Clear();
-                    //            currentNumbers.Add(set[i]);
-                    //            continue;
-                    //        }
-                    //        else if (curSum + set[j] < sum)
-                    //        {
-                    //            currentNumbers.Add(set[j]);
-                    //            curSum += set[j];
-                    //        }
-                    //        else
-                    //        {
-                    //            currentNumbers.Add(set[j]);
-                    //            numSums.Add(currentNumbers);
-                    //            //bool alreadyExists = false;
-                    //            //foreach (List<Int32> existingSet in numSums)
-                    //            //{
-                    //            //    alreadyExists = (existingSet.All(currentNumbers.Contains));
-                    //            //}
-                    //            //if (!alreadyExists)
-                    //            //{
-                    //            //    numSums.Add(currentNumbers);
-                    //            //}
-                    //            break;
-                    //        }
-                    //    }
-                    //}
                 }
             }
 
